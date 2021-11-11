d47a50368601bb816c5d4dc141896bdd62dd5ae8
katsuya ohshima <katsuya_ohshima@jp.ricoh.com>
2021-11-12 01:47:08 +0900

    カメラ入力を画処理できるようにした

--- app/src/main/java/com/example/no_reaction/MainActivity.java
+++ app/src/main/java/com/example/no_reaction/MainActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -33,6 +37,7 @@ import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
@@ -55,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
     // UI
     private TextureView textureView;
     private Button captureButton;
-
+    private ImageView testview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,11 +71,16 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.titleImage).setVisibility(View.VISIBLE);
         findViewById(R.id.titleLabel).setVisibility(View.VISIBLE);
         findViewById(R.id.titleButton).setVisibility(View.VISIBLE);
+
         findViewById(R.id.titleButton).setOnClickListener(this::startAnimation);
 
         // UI
         this.textureView = findViewById(R.id.texture_view);
         this.captureButton = findViewById(R.id.capture_button);
+        this.testview = findViewById(R.id.testview);
+        captureButton.setVisibility(View.GONE);
+
+        captureButton.setOnClickListener(this::capture);
 
         // パーミッションのチェック
         if (allPermissionsGranted()) {
@@ -124,6 +134,7 @@ public class MainActivity extends AppCompatActivity {
                     // SurfaceTextureをTextureViewに指定
                     this.textureView.setSurfaceTexture(output.getSurfaceTexture());
 
+
                     // TextureViewのサイズの調整
                     int w = output.getTextureSize().getWidth();
                     int h = output.getTextureSize().getHeight();
@@ -140,23 +151,72 @@ public class MainActivity extends AppCompatActivity {
                     params.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
                     params.bottomToBottom = ConstraintLayout.LayoutParams.PARENT_ID;
                     textureView.setLayoutParams(params);;
+
                 });
 
         // カメラのライフサイクルのバインド
         CameraX.bindToLifecycle(this, preview);
+
+    }
+
+    Handler handler= new Handler();
+    Boolean captureWkup = false;
+
+    public void capture(View v){
+
+        // 多重起動防止
+        if (captureWkup == true){return;}
+        captureWkup = true;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // マルチスレッドにしたい処理 ここから
+                // 画処理をここに実装する。
+                Bitmap nowView = textureView.getBitmap();
+                Bitmap prevView = nowView;
+                int width = nowView.getWidth();
+                int height = nowView.getHeight();
+                int count = 0;
+                int split = 4;
+
+                Bitmap new_bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas(new_bitmap);
+
+                while (true) {
+
+                    nowView = textureView.getBitmap();
+                    Bitmap line = Bitmap.createBitmap(nowView,0, count, width, split, null, true);
+                    canvas.drawBitmap(line, 0, count, (Paint)null); // image, x座標, y座標, Paintイタンス
+
+                    count = count+split;
+                    if (count+split > height-1){count  = 0;}
+
+                    Bitmap finalNowView = new_bitmap;
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            testview.setImageBitmap(finalNowView);
+                        }
+                    });
+                }
+                // マルチスレッドにしたい処理 ここまで
+            }
+        }).start();
+
     }
 
     public void startAnimation(View v) {
         findViewById(R.id.titleButton).setVisibility(View.GONE);
         findViewById(R.id.titleLabel).setVisibility(View.GONE);
         fadeout(findViewById(R.id.titleImage));
+        findViewById(R.id.capture_button).setVisibility(View.VISIBLE);
     }
 
     public  void fadeout(View target){
         PropertyValuesHolder ani1 = PropertyValuesHolder.ofFloat( "scaleX", 1f, 0f );
         PropertyValuesHolder ani2 = PropertyValuesHolder.ofFloat( "alpha", 1f, 0f );
         ObjectAnimator objectAnimator = ObjectAnimator.ofPropertyValuesHolder(target,ani1,ani2);
-        objectAnimator.setDuration(2000);
+        objectAnimator.setDuration(1000);
         objectAnimator.setRepeatCount(0);//0で1回2
         objectAnimator.start();
     }
